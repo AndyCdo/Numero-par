3f754133d70a7f47e4fbab4569a1025d163f3379
Andrea <chela355@hotmail.com>
2021-03-02 15:25:15 -0500

    se hizo la tarea

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace Numero_par
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ingrese un numero: ");
+            int Numeros= Int32.Parse(Console.ReadLine());
+            bool resultado=Par(Numeros);
+            Console.WriteLine("El numero es " + resultado);
+            
+        }
+        static bool Par(int numero){
+            if (numero % 2 == 0){
+                return true;
+            }
+            else{
+                return false;
+            }
+    
         }
     }
 }
